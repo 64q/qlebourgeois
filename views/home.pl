3fcacf9c0e7f78f41b868ae0b8c9e0743733dd82
64q <quentin.lebourgeois@gmail.com>
2015-03-08 17:13:53 +0100

    fixed typo

--- views/home.pl
+++ views/home.pl
@@ -11,7 +11,7 @@ home_content -->
       'Le site sur lequel vous naviguez actuellement est propulsé grâce au framework web inclus dans Prolog.'
     ]),
     p('Prolog est un langage de programmation logique, proposant une autre approche du travail de développeur.'),
-    p('Le site a été développé autour du fremwork http inclus nativement dans SWI-Prolog 6. Celui-ci permet de construire rapidement un site web.'),
+    p('Le site a été développé autour du framework http inclus nativement dans SWI-Prolog 6. Celui-ci permet de construire rapidement un site web.'),
     h1('Liens utiles'),
     p([
       'Pour en apprendre plus, le site officiel est une mine d\u0027information : ',
