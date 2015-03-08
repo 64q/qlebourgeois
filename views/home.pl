2d27d961c52fb0a5d560d7bb8b510b5b48b017b9
64q <quentin.lebourgeois@gmail.com>
2015-03-08 16:27:13 +0100

    update home view

--- views/home.pl
+++ views/home.pl
@@ -5,13 +5,14 @@
 home_content -->
   html([
     p('Développeur Web chevronné PHP (Symfony2 / Doctrine), Java (Spring / Hibernate) passionné de code.'),
-    p('Retouvez-moi sur les réseaux sociaux à l\u0027aide des liens en haut à droite'),
+    p('Retrouvez-moi sur les réseaux sociaux à l\u0027aide des liens en haut à droite.'),
     h1('Présentation du site'),
     p([
-      'Le site sur lequel vous naviguez actuellement est différent de ce qui se fait actuellment. ',
-      'En effet, celui-ci a entièrement été écrit en Prolog.'
+      'Le site sur lequel vous naviguez actuellement est propulsé grâce au framework web inclus dans Prolog.'
     ]),
     p('Prolog est un langage de programmation logique, proposant une autre approche du travail de développeur.'),
+    p('Le site a été développé autour du fremwork http inclus nativement dans SWI-Prolog 6. Celui-ci permet de construire rapidement un site web.'),
+    h1('Liens utiles'),
     p([
       'Pour en apprendre plus, le site officiel est une mine d\u0027information : ',
       a(href='http://www.swi-prolog.org/', 'http://www.swi-prolog.org/')
